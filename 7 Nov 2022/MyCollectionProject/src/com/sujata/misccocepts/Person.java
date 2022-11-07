64e1ef14d9c49b96676148db766ac7ecf0b1da45
Sujatabatra <sujatabatra@hotmail.com>
2022-11-07 15:31:56 +0530

    Add files via upload

---
+++ 7 Nov 2022/MyCollectionProject/src/com/sujata/misccocepts/Person.java
@@ -0,0 +1,47 @@
+package com.sujata.misccocepts;
+
+import java.util.Objects;
+
+public class Person {
+
+	private int pId;
+	private String pName;
+	public Person() {
+		
+	}
+	public Person(int pId, String pName) {
+		super();
+		this.pId = pId;
+		this.pName = pName;
+	}
+	public int getpId() {
+		return pId;
+	}
+	public void setpId(int pId) {
+		this.pId = pId;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	@Override
+	public String toString() {
+		return "Person [pId=" + pId + ", pName=" + pName + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return pId == other.pId && Objects.equals(pName, other.pName);
+	}
+	
+	
+}
