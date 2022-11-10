8e1ecccd35189b4158bdbef48819e4be2f9e5a95
Sujatabatra <sujatabatra@hotmail.com>
2022-11-10 20:37:34 +0530

    Add files via upload

---
+++ 10 Nov 2022/MyLayeredEmployeeCollectionSpringXMLConfigurationProject/src/com/sujata/entity/Employee.java
@@ -0,0 +1,19 @@
+package com.sujata.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private String empDesignation;
+	private String empDepartment;
+	private double empSalary;
+}
