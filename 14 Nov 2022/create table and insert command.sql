f957158d5d5b77738e1725aa9d1b437a46488232
Sujatabatra <sujatabatra@hotmail.com>
2022-11-14 21:03:55 +0530

    Add files via upload

--- 14 Nov 2022/create table and insert command.sql
+++ 14 Nov 2022/create table and insert command.sql
@@ -37,3 +37,25 @@ insert into employee
 values(104,'DDDD','Manager','Sales',150000),
 (105,'EEEE','Sr. Associate','HR',135000),
 (106,'FFFF','Representative','Sales',25000);
+
+-- command won't work as primary key is duplicate
+insert into employee
+values(105,'Helen','Executive','IT',65000);
+
+insert into employee
+values(107,'Helen','Executive',null,65000);
+
+insert into employee(employeeId,name,salary)
+values(108,"GGGG",89000);
+
+-- Primary key means unique and not null
+insert into employee
+values(null,'Rehan','Executive',null,65000);
+
+
+insert ignore into employee
+values(109,'HHHHH','Manager','IT',250000),
+(105,'IIII','Sr. Associate','HR',135000),
+(110,'IIII','Associate','HR',125000);
+
+select * from employee;
---
+++ 14 Nov 2022/select command.sql
@@ -0,0 +1,7 @@
+select * 
+from employee
+where employeeid=103;
+
+select * 
+from employee
+where department="Sales";
