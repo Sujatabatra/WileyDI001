1a2cd83b5242806f9d4d7ea4af1b3604dfb0c360
Sujatabatra <sujatabatra@hotmail.com>
2022-11-30 16:24:22 +0530

    Add files via upload

---
+++ 30 Nov 2022/employee-layered-springboot-mvc-project/src/main/java/com/sujata/controller/EmployeeController.java
@@ -0,0 +1,42 @@
+package com.sujata.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sujata.model.service.EmployeeService;
+
+@Controller
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@RequestMapping("/")
+	public ModelAndView menuPageController() {
+		return new ModelAndView("index");
+	}
+	@RequestMapping("/deleteEmpPage")
+	public ModelAndView deletePageController() {
+		return new ModelAndView("InputIdForDelete");
+	}
+	
+	@RequestMapping("/deleteEmployee")
+	public ModelAndView deleteEmployeeController(HttpServletRequest request) {
+		ModelAndView modelAndView=new ModelAndView();
+		int eId=Integer.parseInt(request.getParameter("empId"));
+		String message=null;
+		if(employeeService.deleteEmployee(eId))
+			message="Employee Deleted with ID "+eId;
+		else
+			message="Employee with ID "+eId+" doesnot exist";
+		
+		modelAndView.addObject("message", message);
+		modelAndView.setViewName("Output");
+		
+		return modelAndView;
+	}
+}
